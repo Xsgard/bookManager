280de5cc0cde3cc14411a34518a5cb1c39e8ac54
asgard <1792181243@qq.com>
2023-07-27 14:36:30 +0800

    UserService

---
+++ src/main/java/com/xl/BookManager/service/UserService.java
@@ -0,0 +1,8 @@
+package com.xl.BookManager.service;
+
+
+import com.mybatisflex.core.service.IService;
+import com.xl.BookManager.entity.User;
+
+public interface UserService extends IService<User> {
+}
