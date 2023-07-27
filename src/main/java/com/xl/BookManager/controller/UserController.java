c390013d142fbb816145522dc278a6cb2d0b121c
asgard <1792181243@qq.com>
2023-07-27 19:17:13 +0800

    UserController

--- src/main/java/com/xl/BookManager/controller/UserController.java
+++ src/main/java/com/xl/BookManager/controller/UserController.java
@@ -1,11 +1,23 @@
 package com.xl.BookManager.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.xl.BookManager.common.R;
 import com.xl.BookManager.entity.User;
 import com.xl.BookManager.service.UserService;
+import com.xl.BookManager.utils.CheckCodeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 /**
  * @author Asgard
@@ -13,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @description: 处理‘/user’的请求
  * @date 2023/7/27 14:26
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class UserController {
     private UserService userService;
@@ -23,8 +35,37 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/login")
-    public void login(User user) {
+    @RequestMapping(value = "/toLogin", method = RequestMethod.GET)
+    public String toLogin() {
+        return "/user/login";
+    }
 
+    @PostMapping("/login")
+    public String login(@RequestParam("name") String name, @RequestParam("pwd") String password,
+                         Model model, HttpSession session) {
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(StringUtils.isEmpty(name), User::getUserName, name);
+        User user = userService.getOne(queryWrapper);
+        if (user == null) {
+            model.addAttribute("USER_NOT_EXIST", "用户不存在");
+            return "/user/login";
+        }
+        if (!password.equals(user.getPassword())) {
+            model.addAttribute("PWD_ERROR", "密码错误");
+            return "/user/login";
+        }
+        return "book/list";
+    }
+
+    public void register(@RequestBody User user) {
+
+    }
+
+    @RequestMapping(value = "/checkCode", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
+    public void checkCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
+        ServletOutputStream outputStream = response.getOutputStream();
+        String checkCode = CheckCodeUtil.outputVerifyImage(100, 35, outputStream, 4);
+        session.setAttribute("checkCode", checkCode);
     }
 }
