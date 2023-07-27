1bb4fcb6544e2f00533cd5a118a9d772eccfa160
asgard <1792181243@qq.com>
2023-07-27 14:43:35 +0800

    BookService

--- src/main/java/com/xl/BookManager/service/BookService.java
+++ src/main/java/com/xl/BookManager/service/BookService.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 import java.util.List;
 
 /******************
- * @author yejf
+ * @author asgard
  * @description BookService
  */
 public interface BookService extends IService<Book> {
