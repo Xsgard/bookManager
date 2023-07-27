03f38e5c6f51f02c11ed805a47ef403d9b67eb35
asgard <1792181243@qq.com>
2023-07-27 14:36:36 +0800

    BookService

---
+++ src/main/java/com/xl/BookManager/service/BookService.java
@@ -0,0 +1,14 @@
+package com.xl.BookManager.service;
+
+import com.mybatisflex.core.service.IService;
+import com.xl.BookManager.entity.Book;
+
+import java.io.Serializable;
+import java.util.List;
+
+/******************
+ * @author yejf
+ * @description TODO
+ */
+public interface BookService extends IService<Book> {
+}
