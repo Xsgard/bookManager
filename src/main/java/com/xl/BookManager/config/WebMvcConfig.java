3abccf6319884f1f5f96ec9267ba35ace24338cc
asgard <1792181243@qq.com>
2023-07-27 14:35:20 +0800

    WebConfig

---
+++ src/main/java/com/xl/BookManager/config/WebMvcConfig.java
@@ -0,0 +1,80 @@
+package com.xl.BookManager.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/******************
+ * @Author yejf
+ * @Description TODO
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    //@Bean
+    /*public FilterRegistrationBean filterRegistrationBean() {
+        System.out.println("执行了 FilterRegistrationBean...");
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new LoginFilter());
+        filterRegistrationBean.addUrlPatterns("/book/*");
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }*/
+
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean lvfb = new LocalValidatorFactoryBean();
+        //设置属性
+        lvfb.setValidationMessageSource(messageSource());
+        //返回
+        return lvfb;
+    }
+
+    /*****
+     * 用来指定验证时要读取的资源文件
+     * @return
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        //属性
+        messageSource.setBasename("beanValidation");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(60);
+        messageSource.setAlwaysUseMessageFormat(true);
+        //
+        return messageSource;
+    }
+
+    /**
+     * 设置静态资源映射
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/images/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+    /**
+     * 扩展Mvc框架的消息转换器
+     *
+     * @param converters
+     */
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        converters.add(0, messageConverter);
+    }
+}
