637259f6314ca5fa9c94499da897d651be499757
asgard <1792181243@qq.com>
2023-07-27 14:42:43 +0800

    UserServiceImpl

--- src/main/java/com/xl/BookManager/service/impl/UserServiceImpl.java
+++ src/main/java/com/xl/BookManager/service/impl/UserServiceImpl.java
@@ -2,16 +2,16 @@ package com.xl.BookManager.service.impl;
 
 import com.mybatisflex.spring.service.impl.ServiceImpl;
 import com.xl.BookManager.entity.User;
-import com.xl.BookManager.mapper.UserMapper;
 import com.xl.BookManager.service.UserService;
+import com.xl.BookManager.mapper.UserMapper;
 import org.springframework.stereotype.Service;
 
 /**
  * @author Asgard
  * @version 1.0
- * @description: TODO
+ * @description: BookService的实现类，继承了mybatis-flex的ServiceImpl，实现了自定义的UserService
  * @date 2023/7/27 14:14
  */
 @Service
-public class UserServiceImpl extends ServiceImpl<UserMapper,User> implements UserService {
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 }
