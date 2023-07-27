de1c5ca0640fb7015872681f6bd95e7a27339ee6
asgard <1792181243@qq.com>
2023-07-27 19:16:00 +0800

    test

--- src/test/java/com/xl/BookManager/BookManagerApplicationTests.java
+++ src/test/java/com/xl/BookManager/BookManagerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.xl.BookManager;
 
+import com.xl.BookManager.entity.User;
+import com.xl.BookManager.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookManagerApplicationTests {
+    @Autowired
+    private UserService userService;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void readTest() {
+        User byId = userService.getById(12);
+        System.out.println(byId);
     }
 
 }
