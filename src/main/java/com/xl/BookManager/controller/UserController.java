d170c9643bb1c3f820dc6bc6c576ced8daf949e9
asgard <1792181243@qq.com>
2023-07-28 10:28:39 +0800

    UserController

--- src/main/java/com/xl/BookManager/controller/UserController.java
+++ src/main/java/com/xl/BookManager/controller/UserController.java
@@ -6,6 +6,7 @@ import com.xl.BookManager.common.R;
 import com.xl.BookManager.entity.User;
 import com.xl.BookManager.service.UserService;
 import com.xl.BookManager.utils.CheckCodeUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -27,6 +28,7 @@ import java.io.IOException;
  */
 @Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
     private UserService userService;
 
@@ -37,30 +39,60 @@ public class UserController {
 
     @RequestMapping(value = "/toLogin", method = RequestMethod.GET)
     public String toLogin() {
-        return "/user/login";
+        return "user/login";
     }
 
-    @PostMapping("/login")
+    /**
+     * 登录方法
+     *
+     * @param name
+     * @param password
+     * @param checkcode
+     * @param model
+     * @param session
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(@RequestParam("name") String name, @RequestParam("pwd") String password,
-                         Model model, HttpSession session) {
+                        @RequestParam("checkcode") String checkcode, Model model, HttpSession session) throws IOException {
         LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(StringUtils.isEmpty(name), User::getUserName, name);
         User user = userService.getOne(queryWrapper);
         if (user == null) {
             model.addAttribute("USER_NOT_EXIST", "用户不存在");
-            return "/user/login";
+            return "user/login";
         }
         if (!password.equals(user.getPassword())) {
             model.addAttribute("PWD_ERROR", "密码错误");
-            return "/user/login";
+            return "user/login";
         }
-        return "book/list";
+        String code = (String) session.getAttribute("checkCode");
+        if (code.compareToIgnoreCase(checkcode) != 0) {
+            model.addAttribute("CKCODE_ERROR", "验证码错误");
+            return "user/login";
+        }
+        session.setAttribute("LOGIN_USER", user);
+        log.info("用户{}登录成功", user.getUserName());
+        return "redirect:/book/list";
     }
 
     public void register(@RequestBody User user) {
 
     }
 
+    public String logout(HttpSession session) {
+        session.removeAttribute("LOGIN_USER");
+        return "redirect:/user/toLogin";
+    }
+
+    /**
+     * 获取验证码
+     *
+     * @param request
+     * @param response
+     * @throws IOException
+     */
     @RequestMapping(value = "/checkCode", method = RequestMethod.GET, produces = MediaType.IMAGE_JPEG_VALUE)
     public void checkCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
         HttpSession session = request.getSession();
