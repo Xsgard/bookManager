60a5ce349f0ee46333d0ca8030498e6392a1becd
asgard <1792181243@qq.com>
2023-07-28 19:55:08 +0800

    test

--- src/test/java/com/xl/BookManager/BookManagerApplicationTests.java
+++ src/test/java/com/xl/BookManager/BookManagerApplicationTests.java
@@ -4,7 +4,11 @@ import com.xl.BookManager.entity.User;
 import com.xl.BookManager.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.util.UUID;
 
 @SpringBootTest
 class BookManagerApplicationTests {
@@ -19,6 +23,21 @@ class BookManagerApplicationTests {
     void readTest() {
         User byId = userService.getById(12);
         System.out.println(byId);
+    }
+
+    @Test
+    void testUUid() {
+        String uuid = UUID.randomUUID().toString();
+        String substring = uuid.substring(uuid.lastIndexOf("-"));
+        substring = substring.replaceFirst("-", " ").trim();
+        System.out.println(substring);
+    }
+
+    @Test
+    void applicationTest() {
+        ApplicationHome home = new ApplicationHome();
+        File source = home.getSource();
+        System.out.println(source);
     }
 
 }
