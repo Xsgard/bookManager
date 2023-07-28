759781d39897fc81c80428a9c601a10be3c6d2f3
asgard <1792181243@qq.com>
2023-07-28 15:22:37 +0800

    启动器

--- src/main/java/com/xl/BookManager/BookManagerApplication.java
+++ src/main/java/com/xl/BookManager/BookManagerApplication.java
@@ -1,12 +1,15 @@
 package com.xl.BookManager;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class BookManagerApplication {
 
     public static void main(String[] args) {
+        log.info("项目启动成功！");
         SpringApplication.run(BookManagerApplication.class, args);
     }
 
