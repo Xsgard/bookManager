c98d38663294dba9255dfa677cbac80e6172f017
asgard <1792181243@qq.com>
2023-07-29 16:57:17 +0800

    登陆过滤器

--- src/main/java/com/xl/BookManager/filter/LoginCheckFilter.java
+++ src/main/java/com/xl/BookManager/filter/LoginCheckFilter.java
@@ -1,8 +1,16 @@
 package com.xl.BookManager.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.xl.BookManager.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 
 /**
  * @author Asgard
@@ -10,11 +18,68 @@ import java.io.IOException;
  * @description: 登录校验器
  * @date 2023/7/27 14:30
  */
-//@WebFilter(filterName = "Login_Filter", urlPatterns = "/*")
+
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
+@Slf4j
 public class LoginCheckFilter implements Filter {
+    //路径匹配器，支持通配符
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
 
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        //1、获取本次请求的URI
+        String requestURI = request.getRequestURI();
+        //定义不需要处理的请求路径
+        String[] urls = new String[]{
+                "/user/toLogin",
+                "/user/login",
+                "/user/logout",
+                "/user/register",
+                "/static/**",
+                "/templates/user/login.html",
+                "/user/checkCode"
+        };
+
+        //2、判断本次请求是否需要处理
+        boolean check = check(urls, requestURI);
+
+        //3、如果不需要处理，则直接放行
+        if (check) {
+            log.info("本次请求{}不需要处理", requestURI);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //4、判断登录状态，如果已登录，则直接放行
+        if (request.getSession().getAttribute("LOGIN_USER") != null) {
+            User user = (User) request.getSession().getAttribute("LOGIN_USER");
+            log.info("用户已登录，用户为：{}", user.getUserName());
+            filterChain.doFilter(request, response);
+            return;
+        }
+        log.info("用户未登录,跳转至登陆页面！");
+        response.sendRedirect("/user/toLogin");
+    }
+
+    /**
+     * 路径匹配，检查本次请求是否需要放行
+     *
+     * @param urls
+     * @param requestURI
+     * @return
+     */
+    public boolean check(String[] urls, String requestURI) {
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if (match) {
+                return true;
+            }
+        }
+        return false;
     }
 }
