54673bbbd3eefa6fd44a16d677d8c46ab884b002
asgard <1792181243@qq.com>
2023-07-29 11:26:26 +0800

    结果包装类

--- src/main/java/com/xl/BookManager/entity/R.java
+++ src/main/java/com/xl/BookManager/entity/R.java
@@ -6,16 +6,16 @@ package com.xl.BookManager.entity;
  * @description: 封装返回结果对象
  * @date 2023/7/27 16:10
  */
-public class Result {
+public class R {
 
     private String status;
 
     private Object data;
 
-    public Result() {
+    public R() {
     }
 
-    public Result(String status, Object data) {
+    public R(String status, Object data) {
         this.status = status;
         this.data = data;
     }
