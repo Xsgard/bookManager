e898289a08650b12be4576d7149fc0dca3d3239a
asgard <1792181243@qq.com>
2023-07-29 13:30:52 +0800

    日志拦截器

---
+++ src/main/java/com/xl/BookManager/interceptor/LogInterceptor.java
@@ -0,0 +1,27 @@
+package com.xl.BookManager.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/7/29 13:15
+ */
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+}
