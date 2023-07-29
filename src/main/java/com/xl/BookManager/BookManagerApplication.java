ba4b89df22236a86d2ef39899e00633214e2773c
asgard <1792181243@qq.com>
2023-07-29 16:56:32 +0800

    启动器

--- src/main/java/com/xl/BookManager/BookManagerApplication.java
+++ src/main/java/com/xl/BookManager/BookManagerApplication.java
@@ -3,11 +3,14 @@ package com.xl.BookManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @Slf4j
 @EnableTransactionManagement
+@ServletComponentScan
 public class BookManagerApplication {
 
     public static void main(String[] args) {
