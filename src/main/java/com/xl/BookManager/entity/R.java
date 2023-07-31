b5ddde7516816f3fc5fcfd7cfbca2aa4ba2a1689
asgard <1792181243@qq.com>
2023-07-31 13:22:46 +0800

    结果包装类

--- src/main/java/com/xl/BookManager/entity/R.java
+++ src/main/java/com/xl/BookManager/entity/R.java
@@ -1,11 +1,14 @@
 package com.xl.BookManager.entity;
 
+import lombok.Data;
+
 /**
  * @author Asgard
  * @version 1.0
  * @description: 封装返回结果对象
  * @date 2023/7/27 16:10
  */
+@Data
 public class R {
 
     private String status;
@@ -17,22 +20,6 @@ public class R {
 
     public R(String status, Object data) {
         this.status = status;
-        this.data = data;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
         this.data = data;
     }
 }
